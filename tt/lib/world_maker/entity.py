6cc63d7b6967650074ba82b989a81026760e0120
gangjeuk <65859590+gangjeuk@users.noreply.github.com>
2026-03-26 13:48:48 +0900

    feat(lib): changed world maker code to use runtime not raw services

--- tt/lib/world_maker/entity.py
+++ tt/lib/world_maker/entity.py
@@ -1,23 +1,23 @@
-from tt.sdk.environment_service.common.model.component import (
-    MJCFPhysicsComponent,
-    RenderingComponent,
-)
-from tt.sdk.environment_service.common.model.entity import EnvironmentObjectEntity
+from tt.sdk import obtain_runtime
 
 
 class Stuff:
-    def __init__(self, obj_uri_or_prebuilt_name: str, name: str | None = None) -> None:
-        rendering = RenderingComponent("", "")
-        physic = MJCFPhysicsComponent(obj_uri_or_prebuilt_name)
-        self.entity = EnvironmentObjectEntity(
-            rendering=rendering, physics=physic, name=name
-        )
+    def __init__(self, obj_uri_or_prebuilt_name: str, name: str = "") -> None:
+        world_maker = obtain_runtime().world_maker
+        self._entity = world_maker.create_entity(name, obj_uri_or_prebuilt_name, "", "")
 
 
 class Robot:
-    def __init__(self, obj_uri_or_prebuilt_name: str, name: str) -> None:
-        rendering = RenderingComponent("", "")
-        physic = MJCFPhysicsComponent(obj_uri_or_prebuilt_name)
-        self.entity = EnvironmentObjectEntity(
-            rendering=rendering, physics=physic, name=name
-        )
+    def __init__(self, obj_uri_or_prebuilt_name: str, name: str = "") -> None:
+        world_maker = obtain_runtime().world_maker
+        self._entity = world_maker.create_entity(name, obj_uri_or_prebuilt_name, "", "")
+
+
+class Camera:
+    def __init__(self):
+        """Not implemented yet"""
+
+
+class Light:
+    def __init__(self):
+        """Not implemented yet"""
