0c5c46c0189f43dc8b2bfb83a526f0d362f87703
gangjeuk <65859590+gangjeuk@users.noreply.github.com>
2026-05-06 20:35:27 +0900

    feat(sdk): change `IStuffRuntimeOps` interface

--- simulac/sdk/runner_service/common/model/runtime.py
+++ simulac/sdk/runner_service/common/model/runtime.py
@@ -5,8 +5,13 @@ from typing import Callable, Protocol, runtime_checkable
 
 @runtime_checkable
 class IStuffRuntimeOps(Protocol):
+    def get_pos(self) -> tuple[float, float, float]: ...
+    def get_quat(self) -> tuple[float, float, float, float]: ...
+    def get_mass(self) -> float: ...
+    def get_friction(self) -> float: ...
+
     def change_pos(self, pos: tuple[float, float, float]) -> None: ...
-    def change_rot(self, rot: tuple[float, float, float]) -> None: ...
+    def change_quat(self, quat: tuple[float, float, float, float]) -> None: ...
     def change_mass(self, mass: float) -> None: ...
     def change_size(self, size: tuple[float, float, float]) -> None: ...
     def change_fixed(self, is_fixed: bool) -> None: ...
@@ -19,23 +24,27 @@ class StuffRuntime:
         self.id = entity_id
         self._ops = ops
 
+    def get_pos(self) -> tuple[float, float, float]:
+        return self._ops.get_pos()
+
+    def get_quat(self) -> tuple[float, float, float, float]:
+        return self._ops.get_quat()
+
+    def get_mass(self) -> float:
+        return self._ops.get_mass()
+
+    def get_friction(self) -> float:
+        return self._ops.get_friction()
+
     def change_pos(self, pos: tuple[float, float, float]) -> None:
         self._ops.change_pos(pos)
 
-    def change_rot(self, rot: tuple[float, float, float]) -> None:
-        self._ops.change_rot(rot)
+    def change_quat(self, quat: tuple[float, float, float, float]) -> None:
+        self._ops.change_quat(quat)
 
     def change_mass(self, mass: float) -> None:
         self._ops.change_mass(mass)
 
-    def change_size(self, size: tuple[float, float, float]) -> None:
-        self._ops.change_size(size)
-
-    def change_fixed(self, is_fixed: bool) -> None:
-        self._ops.change_fixed(is_fixed)
-
     def change_friction(self, friction: float) -> None:
         self._ops.change_friction(friction)
 
-    def change_density(self, density: float) -> None:
-        self._ops.change_density(density)
