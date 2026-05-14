fc8db61ff2a80588cc770eb16cc890d054684610
gangjeuk <65859590+gangjeuk@users.noreply.github.com>
2026-05-14 16:01:26 +0900

    chore(lib): add `TODO` comment on `world_maker/object.py`

--- simulac/lib/world_maker/object.py
+++ simulac/lib/world_maker/object.py
@@ -45,7 +45,6 @@ if TYPE_CHECKING:
         RandomizableVec3,
         Vec3,
     )
-
 from .entity import (
     ActionT,
     AmbientLight,
@@ -154,6 +153,22 @@ class Environment:
         entity_id: str | None = None,
         description: str | None = None,
     ) -> StuffObject | RobotObject[ActionT] | CameraObject | LightObject:
+        """_summary_
+
+        Args:
+            entity (Stuff | Robot[ActionT] | Camera | LightType): _description_
+            pos (RandomizableVec3, optional): _description_. Defaults to (0, 0, 0).
+            rot (RandomizableVec3, optional): _description_. Defaults to (0, 0, 0).
+            entity_id (str | None, optional): _description_. Defaults to None.
+            description (str | None, optional): _description_. Defaults to None.
+
+        Raises:
+            NotImplementedError: _description_
+
+        Returns:
+            StuffObject | RobotObject[ActionT] | CameraObject | LightObject: _description_
+        """
+
         description = description or ""
 
         if isinstance(entity, Stuff):
@@ -215,6 +230,9 @@ class Environment:
         | LightObject
         | str,
     ) -> None:
+        # TODO: @gangjeuk
+        # [ ] - Remove object
+        # [ ] - Remove relations and constraints connected to object
         pass
 
     def get_object(
@@ -266,6 +284,8 @@ class StuffObject:
 
         self._entity = entity
 
+    # region TODO: @gangjeuk
+    # [ ] - check collider, joint, surface exist
     def collider(self, name: str) -> ColliderRef:
         """When user want to customize collision mesh.
         TODO: @gangjeuk
@@ -366,6 +386,8 @@ class StuffObject:
         if self._entity.id is None:
             raise SimulacBaseError("Entity must be added to Environment first")
         return AnchorRef(self._entity.id, name)
+
+    # end-region
 
     def set_mass(self, mass: RandomizableFloat) -> None:
         # do assertion first
