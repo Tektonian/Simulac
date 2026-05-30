fde9f4e0258da07db0574c4e32dd2d41675115a6
gangjeuk <65859590+gangjeuk@users.noreply.github.com>
2026-05-30 19:52:51 +0900

    feat(sdk): create `MujocoNativeContext`

--- simulac/sdk/runner_service/common/model/context.py
+++ simulac/sdk/runner_service/common/model/context.py
@@ -2,19 +2,20 @@ from __future__ import annotations
 
 from typing import Protocol
 
-from .runtime import StuffRuntime
+from .runtime import RobotRuntime, StuffRuntime
+
+type RuntimeObject = StuffRuntime | RobotRuntime
 
 
 class INativeContext(Protocol):
-    engine: str
     """INativeContext class is for exposing native physics engine context.
     We are going to separate API in four layers
-    
+
     User API, Core Runtime API, Capability API, and Native Escape Hatch API.
-    
+
     Since, we can't provide 100% abstract layer to user we intentionally expose physics engine native layer.
     Four layers will expose APIs like below, and abstraction level we become shallow, as the level increasing.
-    
+
         User API
             Environment
             Stuff / Robot / Camera / Light
@@ -32,12 +33,12 @@ class INativeContext(Protocol):
             deformable
 
         Native Escape Hatch
-            runner.native("mujoco")
-            runner.native("newton")
-            runner.native("genesis")
-    
+            runner.context("mujoco")
+            runner.context("newton")
+            runner.context("genesis")
+
     MUST have fields for each engine
-    
+
     1. mujoco
         - data: mjData
         - model: mjModel
@@ -47,16 +48,16 @@ class INativeContext(Protocol):
     3. gs
         - scene
         - entities
-        
+
     Example
     1. mujoco native access
     ```python
-    with runner.native("mujoco") as mj:
+    with runner.context("mujoco") as mj:
         geom_id = mj.resolve_geom("cup", "body")
         mj.model.geom_friction[geom_id][0] = 0.8
         mj.forward()
     ```
-    
+
     2. Fluid capability
     ```
     class IFluidCapability(Protocol):
@@ -70,19 +71,17 @@ class INativeContext(Protocol):
             entity_id: str | None = None,
         ) -> FluidRuntime: ...
 
-    fluid: IFluidCapability = runner.capability("fluid")
-    
-    # If runner's physic engine is mujoco, then throw error
-    
 
+    fluid: IFluidCapability = runner.capability("fluid")
+
+    # If runner's physic engine is mujoco, then throw error
     ```
     """
 
-    type RuntimeObject = StuffRuntime  # | Robot | etc..
-
     def binding(self, entity: str | RuntimeObject): ...
-    def resolve_body(self, entity: str | RuntimeObject, name: str | None = None): ...
-    def resolve_geom(self, entity: str | RuntimeObject, name: str): ...
-    def resolve_site(self, entity: str | RuntimeObject, name: str): ...
-    def resolve_volume(self, entity: str | RuntimeObject, name: str): ...
-    def forward(self) -> None: ...
+
+    # def resolve_body(self, entity: str | RuntimeObject, name: str | None = None): ...
+    # def resolve_geom(self, entity: str | RuntimeObject, name: str): ...
+    # def resolve_site(self, entity: str | RuntimeObject, name: str): ...
+    # def resolve_volume(self, entity: str | RuntimeObject, name: str): ...
+    # def forward(self) -> None: ...
